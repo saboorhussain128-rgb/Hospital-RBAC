c96943a7b716e4220cb290c166327fa8782b1fdf
MOHD Saboor Hussain <saboorhussain129@gmail.com>
2026-07-06 01:29:24 +0530

    Add hospital admin validation middleware

--- routes/hospitalAdminRoutes.js
+++ routes/hospitalAdminRoutes.js
@@ -1,38 +1,73 @@
 const express = require("express");
 const router = express.Router();
 
+const { validationResult } = require("express-validator");
+
 const hospitalAdminController = require("../controllers/hospitalAdminController");
 
+const {
+    createHospitalAdminValidation
+} = require("../validators/hospitalAdminValidator");
+
 /* DEBUG (REMOVE LATER) */
+
 console.log("hospitalAdminController loaded:", hospitalAdminController);
 
-/* -------------------------
+/* =====================================================
    CREATE HOSPITAL ADMIN PAGE
---------------------------*/
+===================================================== */
+
 router.get(
     "/create-hospital-admin",
     hospitalAdminController.createPage
 );
 
-/* -------------------------
+/* =====================================================
    CREATE HOSPITAL ADMIN
---------------------------*/
+===================================================== */
+
 router.post(
+
     "/create-hospital-admin",
+
+    createHospitalAdminValidation,
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+
+            return res.render("platform/createHospitalAdmin", {
+
+                errors: errors.array(),
+                formData: req.body
+
+            });
+
+        }
+
+        next();
+
+    },
+
     hospitalAdminController.createAdmin
+
 );
 
-/* -------------------------
+/* =====================================================
    VIEW HOSPITAL ADMINS
---------------------------*/
+===================================================== */
+
 router.get(
     "/view-hospital-admin",
     hospitalAdminController.viewAdmins
 );
 
-/* -------------------------
+/* =====================================================
    DELETE HOSPITAL ADMIN
---------------------------*/
+===================================================== */
+
 router.get(
     "/delete-hospital-admin/:id",
     hospitalAdminController.deleteAdmin
