3ebdb88e0b14eb1f0fc85a8be07d949aa6a052b2
MOHD Saboor Hussain <saboorhussain129@gmail.com>
2026-07-06 02:17:17 +0530

    Fix hospital validator name length validation

--- validators/hospitalValidator.js
+++ validators/hospitalValidator.js
@@ -6,6 +6,10 @@ const { body } = require("express-validator");
 
 exports.createHospitalValidation = [
 
+    /* ===============================
+       HOSPITAL NAME
+    =============================== */
+
     body("name")
         .trim()
         .notEmpty()
@@ -13,12 +17,20 @@ exports.createHospitalValidation = [
         .isLength({ min: 3 })
         .withMessage("Hospital name must be at least 3 characters."),
 
+    /* ===============================
+       HOSPITAL ADDRESS
+    =============================== */
+
     body("address")
         .trim()
         .notEmpty()
         .withMessage("Hospital address is required.")
         .isLength({ min: 5 })
         .withMessage("Hospital address must be at least 5 characters."),
+
+    /* ===============================
+       HOSPITAL STATUS
+    =============================== */
 
     body("status")
         .notEmpty()
