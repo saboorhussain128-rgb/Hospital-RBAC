b6a56b873e1e41ad76db485a731cfcca59607342
MOHD Saboor Hussain <saboorhussain129@gmail.com>
2026-07-09 17:26:58 +0530

    Add reusable welcome, forgot password and OTP email services

--- services/emailService.js
+++ services/emailService.js
@@ -8,7 +8,7 @@ Hospital RBAC System
 const { transporter } = require("../config/email");
 
 /* ==================================================
-   SEND EMAIL
+   GENERIC EMAIL SENDER
 ================================================== */
 
 const sendEmail = async ({ to, subject, text }) => {
@@ -49,8 +49,146 @@ const sendEmail = async ({ to, subject, text }) => {
 
 };
 
+/* ==================================================
+   WELCOME EMAIL
+================================================== */
+
+const sendWelcomeEmail = async ({
+
+    hospitalName,
+
+    name,
+
+    email,
+
+    password
+
+}) => {
+
+    const subject = "Welcome to Hospital RBAC";
+
+    const text =
+`Hello ${name},
+
+Welcome to Hospital RBAC.
+
+Your Hospital Admin account has been created successfully.
+
+Hospital
+${hospitalName}
+
+Login Email
+${email}
+
+Temporary Password
+${password}
+
+Login URL
+http://localhost:3000/hospital/login
+
+Please change your password after your first login.
+
+Regards,
+Hospital RBAC Team`;
+
+    return sendEmail({
+
+        to: email,
+
+        subject,
+
+        text
+
+    });
+
+};
+
+/* ==================================================
+   FORGOT PASSWORD EMAIL
+================================================== */
+
+const sendForgotPasswordEmail = async ({
+
+    email,
+
+    resetLink
+
+}) => {
+
+    const subject = "Hospital RBAC Password Reset";
+
+    const text =
+`Hello,
+
+A request has been received to reset your password.
+
+Click the link below.
+
+${resetLink}
+
+If you didn't request this, simply ignore this email.
+
+Regards,
+Hospital RBAC Team`;
+
+    return sendEmail({
+
+        to: email,
+
+        subject,
+
+        text
+
+    });
+
+};
+
+/* ==================================================
+   OTP EMAIL
+================================================== */
+
+const sendOTPEmail = async ({
+
+    email,
+
+    otp
+
+}) => {
+
+    const subject = "Hospital RBAC OTP Verification";
+
+    const text =
+`Hello,
+
+Your OTP Code is
+
+${otp}
+
+This OTP will expire in 10 minutes.
+
+Regards,
+Hospital RBAC Team`;
+
+    return sendEmail({
+
+        to: email,
+
+        subject,
+
+        text
+
+    });
+
+};
+
 module.exports = {
 
-    sendEmail
+    sendEmail,
+
+    sendWelcomeEmail,
+
+    sendForgotPasswordEmail,
+
+    sendOTPEmail
 
 };
